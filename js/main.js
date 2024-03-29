bd4274c4a7a4df23c177e5c0c284f45135a790fb
zafarbek2005 <153996622+zafarbek2005@users.noreply.github.com>
2024-03-29 22:12:49 +0500

    .

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector('.wrapper')
+const loading = document.querySelector('.loading')
+const api = "https://fakestoreapi.com/products"
+  
+
+
+
+
+
+
+
+
+
+
+// ============= Navbar toggle START ================
+const navbarCollection = document.querySelector(".navbar__collection")
+const navbarMenu = document.querySelector(".navbar__menu")
+
+navbarMenu.addEventListener("click", ()=>{
+    navbarCollection.classList.toggle('show')
+})
+// ============= Navbar toggle END ================
